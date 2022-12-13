2e44efe50496491ea3cea0ba33a23638f6919171
Italo Teofilo Filho <73993878+papitalos@users.noreply.github.com>
2022-12-13 07:23:25 -0300

    Add files via upload

---
+++ ApostaCorrida.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseProject
+{
+    class ApostaCorrida
+    {
+        private List<Cavalo> listaCavalos;
+        private string nomeApostador;
+        private int valorApostado;
+
+        public ApostaCorrida()
+        {
+            this.listaCavalos = new List<Cavalo>();
+        }
+
+        
+        public string NomeApostador { get => nomeApostador; set => nomeApostador = value; }
+        public int ValorApostado { get => valorApostado; set => valorApostado = value; }
+        internal List<Cavalo> ListaCavalos { get => listaCavalos; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApostaCorrida aposta1 = new ApostaCorrida();
+            Pista Birmighan =  new Pista();
+
+            Birmighan.CicloDiario();
+
+
+        }
+    }
+}
